75e8ed9ff4fe0d0c54706f8ceff71b73977617c8
Dominik Rys <dominik.j.rys@gmail.com>
2021-06-24 20:08:14 +0100

    Initial commit

---
+++ crawler.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+func crawl() {
+	collector := colly.NewCollector(
+		colly.AllowedDomains("imdb.com", "www.imdb.com"),
+		colly.Async(true),
+	)
+
+	infoCollector := collector.Clone()
+
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting: ", request.URL.String())
+	})
+
+	infoCollector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting Profile URL: ", request.URL.String())
+	})
+
+	collector.OnHTML("a.lister-page-next", func(element *colly.HTMLElement) {
+		nextPage := element.Request.AbsoluteURL(element.Attr("href"))
+		collector.Visit(nextPage)
+	})
+
+	collector.OnHTML(".mode-detail", func(element *colly.HTMLElement) {
+		profileHref := element.ChildAttr("div.lister-item-image > a", "href")
+		profileUrl := element.Request.AbsoluteURL(profileHref)
+		infoCollector.Visit(profileUrl)
+	})
+
+	infoCollector.OnHTML("#content-2-wide", func(element *colly.HTMLElement) {
+		profile := Star{}
+		profile.Name = element.ChildText("h1.header > span.itemprop")
+		profile.Photo = element.ChildAttr("#name-poster", "src")
+		profile.JobTitle = element.ChildText("#name-job-categories > a > span.itemprop")
+		profile.BirthDate = element.ChildAttr("#name-born-info time", "datetime")
+		profile.Bio = strings.TrimSpace(element.ChildText("#name-bio-text > div.name-trivia-bio-text > div.inline"))
+
+		element.ForEach("div.knownfor-title", func(_ int, knownForElem *colly.HTMLElement) {
+			movie := Movie{}
+			movie.Title = knownForElem.ChildText("div.knownfor-title-role > a.knownfor-ellipsis")
+			movie.Year = knownForElem.ChildText("div.knownfor-year > span.knownfor-ellipsis")
+
+			profile.TopMovies = append(profile.TopMovies, movie)
+		})
+
+		profileJson, err := json.MarshalIndent(profile, "", "    ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(profileJson))
+	})
+
+	collector.Visit("https://www.imdb.com/search/name/?birth_monthday=12-20")
+}
+
+type Movie struct {
+	Title string
+	Year  string
+}
+
+type Star struct {
+	Name      string
+	Photo     string
+	JobTitle  string
+	BirthDate string
+	Bio       string
+	TopMovies []Movie
+}
+
+func main() {
+
+}
